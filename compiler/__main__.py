09c49a30505a87ca66d22cb5ea5d313b2c806adf
Steven <52606428+cperkkk@users.noreply.github.com>
2026-04-07 21:46:15 -0500

    feat: add compiler backend (#7)
    * feat: add compiler backend and some tests
    
    * chore: refactor
    
    ---------

--- compiler/__main__.py
+++ compiler/__main__.py
@@ -1,17 +1,17 @@
 import sys
-from .pipeline import compiler
+from pathlib import Path
+
+from .pipeline import compile_to_op
 
 
 def main():
     if len(sys.argv) != 2:
         print("usage: python -m compiler <input.py>", file=sys.stderr)
         sys.exit(1)
-    file_path = sys.argv[1]
-    with open(file_path) as file:
-        prog = file.read()
-    ops = compiler(prog)
-    for op in ops:
-        print(op)
+    input_path = Path(sys.argv[1])
+    output_path = input_path.with_suffix(".op")
+    program = input_path.read_text()
+    output_path.write_text(compile_to_op(program) + "\n")
 
 
 if __name__ == "__main__":
--- compiler/pipeline.py
+++ compiler/pipeline.py
@@ -1,8 +1,18 @@
 import ast
-from .op import Op
+
+from .backend import Backend
 from .flattener import Flattener
+from .op import Op
 
 
-def compiler(source: str) -> list[Op]:
+def compile_first_stage(source: str) -> list[Op]:
     tree = ast.parse(source)
     return Flattener().run(tree)
+
+
+def compile_second_stage(flattened_ops: list[Op]) -> str:
+    return Backend().run(flattened_ops)
+
+
+def compile_to_op(source: str) -> str:
+    return compile_second_stage(compile_first_stage(source))
